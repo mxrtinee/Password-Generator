08704ba1ad83b45cc0fd0c2c63db146826f40c0b
Martin <mxrtinee@user.noreply.github.com>
2023-08-07 22:02:07 -0700

    Added Function to generate a random password

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,57 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Function to generate a random password
+function generatePassword() {
+  // Prompt for password length
+  var length = parseInt(prompt("Enter password length (8 to 128 characters):"));
+
+  // Validate the input for password length
+  if (isNaN(length) || length < 8 || length > 128) {
+    alert("Please enter a valid password length (8 to 128 characters).");
+    return '';
+  }
+
+  // Prompt for character types to include
+  var includeLowercase = confirm("Would you like to include lowercase letters? Okay for True & Cancel for False");
+  var includeUppercase = confirm("Would you like to include uppercase letters? Okay for True & Cancel for False");
+  var includeNumbers = confirm("Would you like to include numbers? Okay for True & Cancel for False");
+  var includeSpecial = confirm("Would you like to include special characters? Okay for True & Cancel for False");
+
+  // Validate that at least one character type is selected
+  if (!includeLowercase && !includeUppercase && !includeNumbers && !includeSpecial) {
+    alert("You must include at least one character type in the password.");
+    return '';
+  }
+
+  // Define character sets based on selected criteria
+  var lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+  var uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var numberChars = '0123456789';
+  var specialChars = '!@#$%^&*()-_=+"';
+
+  var chars = '';
+  if (includeLowercase) chars += lowercaseChars;
+  if (includeUppercase) chars += uppercaseChars;
+  if (includeNumbers) chars += numberChars;
+  if (includeSpecial) chars += specialChars;
+
+  // Generate the password
+  var password = '';
+  for (var i = 0; i < length; i++) {
+    var randomIndex = Math.floor(Math.random() * chars.length);
+    password += chars[randomIndex];
+  }
+
+  return password;
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Password-Generator
 This password generator enables the end user to generate random passwords based on criteria that they're selected. This app will run in the browser and will feature dynamically updated HTML and CSS powered by JavaScript code.
+
 ## User Story
 
 ```
